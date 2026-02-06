db2d58bc84c72a4e75d143011b67a4840f79af76
Khang Pham <phamthekhang632@gmail.com>
2026-02-06 07:46:27 +0100

    feat: reinitiallize ur10 position and move it back and forth

--- src/Ur10Learning.cpp
+++ src/Ur10Learning.cpp
@@ -3,21 +3,61 @@
 Ur10Learning::Ur10Learning(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config)
 : mc_control::MCController(rm, dt)
 {
+  config_.load(config);
+
   solver().addConstraintSet(contactConstraint);
-  solver().addConstraintSet(kinematicsConstraint);
+  solver().addConstraintSet(selfCollisionConstraint);
+  solver().addConstraintSet(dynamicsConstraint);
+  solver().addConstraintSet(compoundJointConstraint);
   solver().addTask(postureTask);
-  solver().setContacts({{}});
+  postureTask->stiffness(1);
+  postureTask->weight(1);
+
+  moveTask_ = mc_tasks::MetaTaskLoader::load<mc_tasks::EndEffectorTask>(
+    solver(), config("MoveTask"));
 
   mc_rtc::log::success("Ur10Learning init done ");
 }
 
 bool Ur10Learning::run()
 {
+  if (phase_ == IDLE)
+  {
+    postureTask->target
+    ({
+      {"shoulder_pan_joint", {0.0}}, 
+      {"shoulder_lift_joint", {-M_PI * 5/6}},
+      {"elbow_joint", {M_PI * 2/3}}
+    });
+
+    moveTask_->selectUnactiveJoints(solver(), joints_);
+
+    if (postureTask->eval().norm() < 0.1 && postureTask->speed().norm() < 0.1)
+    {
+      phase_ = MOVE_BACKWARD;
+      moveTask_->selectActiveJoints(solver(), joints_);
+    }
+  }
+  else if (phase_ == MOVE_BACKWARD 
+    && moveTask_->eval().norm() < 0.01 && moveTask_->speed().norm() < 0.1)
+  {
+    phase_ = MOVE_FORWARD;
+    moveTask_->add_ef_pose({Eigen::Vector3d(-0.2, 0.0, 0.0)});
+  }
+  else if (phase_ == MOVE_FORWARD
+    && moveTask_->eval().norm() < 0.01 && moveTask_->speed().norm() < 0.1)
+  {
+    phase_ = MOVE_BACKWARD;
+    moveTask_->add_ef_pose({Eigen::Vector3d(0.2, 0.0, 0.0)});
+  }
   return mc_control::MCController::run();
 }
 
 void Ur10Learning::reset(const mc_control::ControllerResetData & reset_data)
 {
+  moveTask_->reset();
+  solver().addTask(moveTask_);
+
   mc_control::MCController::reset(reset_data);
 }
 
--- src/Ur10Learning.h
+++ src/Ur10Learning.h
@@ -1,15 +1,29 @@
 #pragma once
 
 #include <mc_control/mc_controller.h>
-
+#include <mc_tasks/EndEffectorTask.h>
+#include <mc_tasks/MetaTaskLoader.h>
 
 #include "api.h"
+
+enum ControlState
+{
+  IDLE = 0,
+  MOVE_BACKWARD,
+  MOVE_FORWARD
+};
 
 struct Ur10Learning_DLLAPI Ur10Learning : public mc_control::MCController
 {
   Ur10Learning(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config);
-
   bool run() override;
-
   void reset(const mc_control::ControllerResetData & reset_data) override;
+  
+  private:
+    mc_rtc::Configuration config{};
+    std::shared_ptr<mc_tasks::EndEffectorTask> moveTask_;
+    ControlState phase_ = IDLE;
+    std::vector<std::string> joints_ = {
+      "shoulder_pan_joint", "shoulder_lift_joint", "elbow_joint",
+      "wrist_1_joint",      "wrist_2_joint",       "wrist_3_joint"};
 };
