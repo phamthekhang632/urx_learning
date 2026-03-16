1c3bc9070b3546aeafae879cea818dabeb92a577
Khang Pham <phamthekhang632@gmail.com>
2026-03-16 12:23:54 +0900

    feat: add resting pose

--- src/UrXLearning.cpp
+++ src/UrXLearning.cpp
@@ -1,15 +1,13 @@
 #include "UrXLearning.h"
 
 #include <mc_rbdyn/RobotLoader.h>
-#include <thread>
 #include <chrono>
+#include <thread>
 
-UrXLearning::UrXLearning(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration &config)
-    : mc_control::MCController(
-          {rm,
-           mc_rbdyn::RobotLoader::get_robot_module("robotiq_arg85"),
-           mc_rbdyn::RobotLoader::get_robot_module("env/ground")},
-          dt)
+UrXLearning::UrXLearning(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config)
+: mc_control::MCController({rm, mc_rbdyn::RobotLoader::get_robot_module("robotiq_arg85"),
+                            mc_rbdyn::RobotLoader::get_robot_module("env/ground")},
+                           dt)
 {
   solver().addConstraintSet(contactConstraint);
   solver().addConstraintSet(selfCollisionConstraint);
@@ -23,19 +21,21 @@ UrXLearning::UrXLearning(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::C
 
 bool UrXLearning::run()
 {
-  if (postureTask->eval().norm() < 0.02 && postureTask->speed().norm() < 0.01)
+  if(postureTask->eval().norm() < 0.02 && postureTask->speed().norm() < 0.01)
   {
-    switch_target();
+    if(false)
+      switch_target();
+    else
+      store_target();
   }
   return mc_control::MCController::run();
 }
 
-void UrXLearning::reset(const mc_control::ControllerResetData &reset_data)
+void UrXLearning::reset(const mc_control::ControllerResetData & reset_data)
 {
   mc_control::MCController::reset(reset_data);
   // robot(1) = robotiq_arg85
-  robots().robot(1).posW(
-      sva::PTransformd(sva::RotX(-M_PI / 2), Eigen::Vector3d(0.8172, 0.2329, 0.0628)));
+  robots().robot(1).posW(sva::PTransformd(sva::RotX(-M_PI / 2), Eigen::Vector3d(0.8172, 0.2329, 0.0628)));
   addContact({"ur5e", "robotiq_arg85", "Tool", "Base"});
 
   gripperPostureTask_ = std::make_shared<mc_tasks::PostureTask>(solver(), 1);
@@ -47,25 +47,41 @@ CONTROLLER_CONSTRUCTOR("UrXLearning", UrXLearning)
 void UrXLearning::switch_target()
 {
   std::map<std::string, std::vector<double>> jointTarget;
-  switch (phase_)
+  switch(phase_)
   {
-  case IDLE:
-    postureTask->target(defaultPosture);
-    phase_ = MOVE_UP;
-    break;
+    case IDLE:
+      postureTask->target(defaultPosture);
+      phase_ = MOVE_UP;
+      break;
 
-  case MOVE_UP:
-    jointTarget[moveJoint] = {jointAngDefault + moveMag};
-    postureTask->target(jointTarget);
-    gripperPostureTask_->target(gripperClose);
-    phase_ = MOVE_DOWN;
-    break;
+    case MOVE_UP:
+      jointTarget[moveJoint] = {jointAngDefault + moveMag};
+      postureTask->target(jointTarget);
+      gripperPostureTask_->target(gripperClose);
+      phase_ = MOVE_DOWN;
+      break;
 
-  case MOVE_DOWN:
-    jointTarget[moveJoint] = {jointAngDefault - moveMag};
-    postureTask->target(jointTarget);
-    gripperPostureTask_->target(gripperOpen);
-    phase_ = MOVE_UP;
-    break;
+    case MOVE_DOWN:
+      jointTarget[moveJoint] = {jointAngDefault - moveMag};
+      postureTask->target(jointTarget);
+      gripperPostureTask_->target(gripperOpen);
+      phase_ = MOVE_UP;
+      break;
+  }
+}
+
+void UrXLearning::store_target()
+{
+  // std::map<std::string, std::vector<double>> jointTarget;
+  switch(store_state_)
+  {
+    case START:
+      postureTask->target(storePoseStart);
+      store_state_ = TURN;
+      break;
+
+    case TURN:
+      postureTask->target(storePoseTurn);
+      break;
   }
 }
--- src/UrXLearning.h
+++ src/UrXLearning.h
@@ -10,11 +10,17 @@ enum ControlState
   MOVE_DOWN
 };
 
+enum StoreState
+{
+  START = 0,
+  TURN
+};
+
 struct UrXLearning_DLLAPI UrXLearning : public mc_control::MCController
 {
-  UrXLearning(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration &config);
+  UrXLearning(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config);
   bool run() override;
-  void reset(const mc_control::ControllerResetData &reset_data) override;
+  void reset(const mc_control::ControllerResetData & reset_data) override;
 
 private:
   void switch_target();
@@ -25,15 +31,18 @@ private:
   double jointAngDefault = 0;
   double moveMag = M_PI / 6;
   std::map<std::string, std::vector<double>> defaultPosture = {
-      {"shoulder_pan_joint", {25 * M_PI / 180}},
-      {"shoulder_lift_joint", {-M_PI / 2}},
-      {"elbow_joint", {-M_PI / 2}},
-      {"wrist_1_joint", {jointAngDefault}},
-      {"wrist_2_joint", {M_PI / 2}},
-      {"wrist_3_joint", {0}}};
+      {"shoulder_pan_joint", {25 * M_PI / 180}}, {"shoulder_lift_joint", {-M_PI / 2}}, {"elbow_joint", {-M_PI / 2}},
+      {"wrist_1_joint", {jointAngDefault}},      {"wrist_2_joint", {M_PI / 2}},        {"wrist_3_joint", {0}}};
 
   // robotiq_arg85
   std::map<std::string, std::vector<double>> gripperOpen = {{"finger_joint", {0.0}}};
   std::map<std::string, std::vector<double>> gripperClose = {{"finger_joint", {0.725}}};
   std::shared_ptr<mc_tasks::PostureTask> gripperPostureTask_;
+
+  void store_target();
+  StoreState store_state_ = START;
+  std::map<std::string, std::vector<double>> storePoseStart = {{"shoulder_lift_joint", {-M_PI}},
+                                                               {"elbow_joint", {0.85 * M_PI}},
+                                                               {"wrist_1_joint", {-M_PI}}};
+  std::map<std::string, std::vector<double>> storePoseTurn = {{"shoulder_pan_joint", {-0.35}}};
 };
